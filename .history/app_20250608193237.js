e2f86e4b96e46c4ad393b56a6523e705350d717a
Igor <freitascasteloigor@gmail.com>
2025-06-08 19:38:04 -0300

    Create main route and setup express-handlebars

---
+++ .history/app_20250608193237.js
@@ -0,0 +1,39 @@
+//Importar modulo express
+const e = require('express');
+const express = require('express');
+
+//Importar modudo espress-handlebars
+const {engine} = require('express-handlebars');
+
+//importar modulo  mysql
+const mysql = require('mysql2');
+
+//app
+const app = express();
+
+// Configuração do express-handlebars
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+// Configuração de conecxão com o banco de dados
+const conecxao = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '123456',
+    database: 'projeto'
+});
+
+//teste de conexão
+conecxao.connect((erro) => {
+    if (erro) throw erro;
+    console.log('Conexão efetuada com sucesso!');
+});
+
+//Rota principal
+app.get('/', function(req, res) {
+    res.render('formulario');
+});
+
+app.listen(8080);
+
